3951e33b468fb653f48290df26015a6239077595
William Friedrich <willwall916@gmail.com>
2018-05-27 19:36:23 -0700

    Initial Commit

---
+++ HelloWOrld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWOrld
+//
+//  Created by William Friedrich on 5/27/18.
+//  Copyright © 2018 William Friedrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
